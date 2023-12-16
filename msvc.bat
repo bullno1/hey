98c07fa9ab3d663630db14309ce681c7cf59f0ea
Bach Le <bach@bullno1.com>
2023-12-17 06:03:29 +0800

    MSVC: Use newer SDK version

--- msvc.bat
+++ msvc.bat
@@ -4,13 +4,13 @@ for /f "usebackq tokens=*" %%i in (`cmake\vswhere -latest -prerelease -property 
   set VS_DIR=%%i
 )
 
-call "%VS_DIR%\VC\Auxiliary\Build\vcvarsall.bat" x64 10.0.22000.0
+call "%VS_DIR%\VC\Auxiliary\Build\vcvarsall.bat" x64 10.0.22621.0
 
 mkdir msvc
 cd msvc
 cmake ^
     -G "Visual Studio 17 2022" ^
     -A x64 ^
-    -D CMAKE_SYSTEM_VERSION=10.0.22000.0 ^
+    -D CMAKE_SYSTEM_VERSION=10.0.22621.0 ^
     -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ^
     ..
