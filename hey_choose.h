bbbbfa7de7dca175299ed181af9543a5ec79008c
Bach Le <bach@bullno1.com>
2024-05-21 22:28:49 +0800

    Update hey_choose to do prefix match too

--- hey_choose.h
+++ hey_choose.h
@@ -32,6 +32,12 @@ hey_choose_controller(hey_index_t* count, hey_exec_t* ctx, void* userdata) {
 	};
 	if (ctx_str.length < 0) { return HEY_CONTINUE; }
 
+#if 0
+	printf("\n---\n");
+	printf("Context: {%.*s}\n", ctx_str.length, ctx_str.chars);
+	printf("---\n");
+#endif
+
 	for (hey_index_t i = 0; choose_state->options[i].length > 0; ++i) {
 		hey_str_t option = choose_state->options[i];
 
@@ -56,10 +62,21 @@ hey_choose_logit_processor(
 	hey_choose_state_t* choose_state = userdata;
 	const hey_state_t* hey_state = hey_get_state(ctx);
 
+	hey_str_t prefix = {
+		.chars = hey_state->text + choose_state->placement_offset,
+		.length = hey_state->num_chars - choose_state->placement_offset,
+	};
+
 	for (hey_token_t token = 0; token < num_logits; ++token) {
 		if (logits[token] == HEY_LOGIT_IGNORE) { continue; }
 
 		hey_str_t token_str = hey_detokenize(ctx, token);
+
+		// Skip 0-length token or the loop will never progress
+		if (token_str.length == 0) {
+			logits[token] = HEY_LOGIT_IGNORE;
+			continue;
+		}
 
 		// Make sure we get past the healing prefix
 		token_str.chars += hey_state->healing_prefix.length;
@@ -70,17 +87,39 @@ hey_choose_logit_processor(
 		bool matched = false;
 		for (hey_index_t i = 0; choose_state->options[i].chars != NULL; ++i) {
 			hey_str_t option = choose_state->options[i];
+			hey_str_t option_suffix = option;
 
-			// It's safe to modify since this is a copy
 			if (hey_state->num_chars > choose_state->placement_offset) {
-				option.chars += hey_state->num_chars - choose_state->placement_offset;
-				option.length -= hey_state->num_chars - choose_state->placement_offset;
+				// Skip options which does not match the prefix
+				hey_index_t cmp_len = HEY_MIN(prefix.length, option.length);
+				if (HEY_STRNCMP(option.chars, prefix.chars, cmp_len) != 0) {
+					continue;
+				}
 
-				if (option.length <= 0) { continue; }
+				// Skip options which cannot get past the placement offset
+				option_suffix.length -= hey_state->num_chars - choose_state->placement_offset;
+
+				if (option_suffix.length <= 0) {
+					continue;
+				}
+				option_suffix.chars += hey_state->num_chars - choose_state->placement_offset;
 			}
 
-			hey_index_t cmp_len = HEY_MIN(option.length, token_str.length);
-			if (HEY_STRNCMP(option.chars, token_str.chars, cmp_len) == 0) {
+			// Skip tokens which is shorter than token
+			if (token_str.length > option.length) {
+				continue;
+			}
+
+			hey_index_t cmp_len = HEY_MIN(option_suffix.length, token_str.length);
+			if (HEY_STRNCMP(option_suffix.chars, token_str.chars, cmp_len) == 0) {
+#if 0
+				printf("\n---\n");
+				printf("Token: {%.*s}\n", token_str.length, token_str.chars);
+				printf("Option: {%.*s}\n", option.length, option.chars);
+				printf("Option suffix: {%.*s}\n", option_suffix.length, option_suffix.chars);
+				printf("---\n");
+#endif
+
 				matched = true;
 				break;
 			}
@@ -96,6 +135,8 @@ hey_index_t
 hey_choose(hey_exec_t* ctx, const hey_str_t* options) {
 	const hey_state_t* hey_state = hey_get_state(ctx);
 
+	// TODO: Initialize placement offset on first controller call
+	// Healing is not initialized until hey_generate is called
 	hey_choose_state_t choose_state = {
 		.choice = -1,
 		.options = options,
